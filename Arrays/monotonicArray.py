c823060ec16981d57e01d1667667d64af4cc2858
blessdog <waterbreathers@gmail.com>
2023-08-20 19:57:59 -0600

    isMonotonic

---
+++ Arrays/monotonicArray.py
@@ -0,0 +1,12 @@
+'''
+Write a function that takes in an array of integers and returns a booly representing 
+weather the array is monotonic. 
+An array is monotonic if its elements, from left to right, are entirely non-increasing or entirely 
+non-decreasing. 
+Non-increasing elements arent necessarily exclusivly decreasing; they simply dont increase. Similarly 
+with non-decreasing. 
+Note that empty arrays and arrays of one element are monotonic. 
+'''
+
+def isMonotonic(array):
+
